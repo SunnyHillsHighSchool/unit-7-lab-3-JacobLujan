952ffd07d23e567db89b308dec1a1f3d81ea912d
Jacob Lujan <600022570@fjuhsd.org>
2021-02-26 00:09:48 +0000

    Updated ArrayListFunHouse.java

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -12,10 +12,26 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
-  return null;
+   ArrayList<Integer> funHouse = new ArrayList<Integer>();
+   for(int x = 2; x < number; x++)
+   {
+     if(number % x == 0)
+     {
+       funHouse.add(x);
+     }
+   }
+   return funHouse;
  }
  
  public static void keepOnlyCompositeNumbers( List<Integer> nums )
  {
+   for(int y = nums.size() - 1; y >= 0; y--)
+   {
+     ArrayList<Integer> composite = getListOfFactors(nums.get(y));
+     if(composite.size() == 0)
+     {
+       nums.remove(nums.get(y));
+     }
+   }
  }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jacob Lujan
 
 import java.util.Arrays;
 import java.util.ArrayList;
